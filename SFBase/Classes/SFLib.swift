53452e68b2e2b6834e028809d983839b04754180
hsf89757 <hsf89757@gmail.com>
2024-11-23 12:36:54 +0800

    SFLib

---
+++ SFBase/Classes/SFLib.swift
@@ -0,0 +1,15 @@
+//
+//  SFLib.swift
+//  SFBase
+//
+//  Created by hsf on 2024/11/23.
+//
+
+import Foundation
+// Basic
+import SFExtension
+
+// MARK: - SFBaseLib
+public class SFBaseLib: SFLib {
+    public static var bundle: Bundle? = Bundle.sf.bundle(cls: SFBaseLib.self, resource: nil)
+}
