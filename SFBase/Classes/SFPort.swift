025fdfdd73c27c93cba08f9253eb9a56fa7fc033
hsf89757 <hsf89757@gmail.com>
2024-12-09 11:16:20 +0800

    优化logger

--- SFBase/Classes/SFPort.swift
+++ SFBase/Classes/SFPort.swift
@@ -9,6 +9,7 @@ import Foundation
 
 // MARK: - SFPort
 public enum SFPort: CaseIterable {
+    case none
     case client    // 客户端
     case server    // 服务端
 }
@@ -17,6 +18,8 @@ extension SFPort {
     /// 本地路径
     public var path: String {
         switch self {
+        case .none:
+            return "none"
         case .client:
             return "client"
         case .server:
